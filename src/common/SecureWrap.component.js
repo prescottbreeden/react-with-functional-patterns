495abba8425c4eac7122f00ef2948e0086be1132
prescottbreeden <prescott.breeden@gmail.com>
2021-02-14 22:17:33 -0800

    updated secure wrap

--- src/common/SecureWrap.component.js
+++ src/common/SecureWrap.component.js
@@ -1,22 +1,23 @@
 import React from "react";
-import { always, compose, cond as firstMatch, equals, includes, prop } from 'ramda';
+import * as R from "ramda";
 
-const isDisabled = firstMatch([
-  [compose(equals('admin'), prop('role')), always(false)],
-  [compose(includes('full'), prop('access')), always(false)],
-  [compose(includes('write'), prop('access')), always(false)],
-  [always(true), always(true)],
+const isDisabled = R.cond([
+  [R.compose(R.equals("admin"), R.prop("role")), R.always(false)],
+  [R.compose(R.includes("full"), R.prop("access")), R.always(false)],
+  [R.compose(R.includes("write"), R.prop("access")), R.always(false)],
+  [R.always(true), R.always(true)],
 ]);
 
-const isHidden = firstMatch([
-  [compose(equals([]), prop('access')), always(true)],
-  [always(true), always(false)],
+const isHidden = R.cond([
+  [R.compose(R.equals([]), R.prop("access")), R.always(true)],
+  [R.always(true), R.always(false)],
 ]);
 
 export const SecureWrap = (props) => {
-  const disabled = isDisabled(props.permission)
+  const disabled = isDisabled(props.permission);
   const mapChildren = (children, fn) => {
     return React.Children.map(children, (child) => {
+      // disable :: child element -> child element
       const disable = (child) => fn(child, { ...child.props, disabled });
       return child.props.children
         ? mapChildren(child.props.children, fn)
@@ -24,10 +25,12 @@ export const SecureWrap = (props) => {
     });
   };
 
-  return isHidden(props.permission)
-    ? <p>
-        You do not have access to view this page. If you believe this is a
-        mistake, please contact a site administrator.
-      </p>
-    : mapChildren(props.children, React.cloneElement);
+  return isHidden(props.permission) ? (
+    <p>
+      You do not have access to view this page. If you believe this is a
+      mistake, please contact a site administrator.
+    </p>
+  ) : (
+    mapChildren(props.children, React.cloneElement)
+  );
 };
